3d040c4db7e302a1c463be366269a4608c3497a7
Christiano Teixeira <chriteixeira@gmail.com>
2016-07-25 21:11:02 -0300

    Better crawler

--- NodeCrawler.js
+++ NodeCrawler.js
@@ -2,16 +2,17 @@
 const fs = require("fs");
 const request = require("request");
 const cheerio = require("cheerio");
+const async = require("async");
 
 var NodeCrawler = function(hostname, maxDepth, currentDepth){
     this.hostname = hostname;
     this.url = "http://"+hostname;
     this.maxDepth = maxDepth;
     this.currentDepth = currentDepth;
-    this.child = [];
+    this.childs = [];
 };
 
-NodeCrawler.prototype.start = function(){
+NodeCrawler.prototype.start = function(callback){
     var self = this;
     var options = {
         url: this.url,
@@ -19,21 +20,39 @@ NodeCrawler.prototype.start = function(){
     }
     request(options, function(err, response, html){
         var self1 = self;
-        if(err) console.log("error reading "+self.url+" "+err);
+        if(err) console.error("error reading "+self.url+" "+err);
         else{
             self.response = response;
             self.html = html;
             var $ = cheerio.load(html);
             $('a').each(function(i,e){
-                var href = $(e).attr('href');
-                if(href !== undefined && href.indexOf('http') < 0 && href.indexOf('html') >= 0){
-                    console.log(href);
-                    console.log(response.request.path);
+                if($(e).attr('href') != undefined){
+                    var href = removeAnchor($(e).attr('href'));
+                    var path = response.request.path;
+                    if(href !== undefined && href.indexOf('http') < 0 && href.indexOf('html') >= 0 && self.childs.indexOf(href) < 0 && href.indexOf(path.substring(0,path.length-1)) < 0){
+                        self.childs.push(href);
+                        callback(self,href);
+                    }
                 }
             });
         }
     });  
 };
 
-var c = new NodeCrawler("coca-cola.com.br",1,1);
-c.start();
+function removeAnchor(str){
+    return str.substring(0, str.indexOf('#'));
+}
+
+var array = fs.readFileSync('sites.txt').toString().split("\r\n");
+var result = '';
+async.each(array, function(e,callback){
+    console.log(e);
+    var c = new NodeCrawler(e,1,1);
+    c.start(saveToFile);
+    setTimeout(callback(), 3000);
+});
+
+
+function saveToFile(c,href){
+    console.log(c.url+href);
+}
